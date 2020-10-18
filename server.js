9f5e6a416adda78e147308627a901d4fd0da8628
T Brichetto <TaraPersonal@MacBook-Air.local>
2020-10-17 22:09:17 -0700

    12.3.5

--- server.js
+++ server.js
@@ -37,8 +37,12 @@ app.use(express.json());
 
 // GET single candidate
 app.get('/api/candidate/:id', (req, res) => {
-  const sql = `SELECT * FROM candidates 
-               WHERE id = ?`;
+  const sql = `SELECT candidates.*, parties.name 
+             AS party_name 
+             FROM candidates 
+             LEFT JOIN parties 
+             ON candidates.party_id = parties.id 
+             WHERE candidates.id = ?`;
   const params = [req.params.id];
   db.get(sql, params, (err, row) => {
     if (err) {
@@ -55,7 +59,11 @@ app.get('/api/candidate/:id', (req, res) => {
 
 // Get all candidates
 app.get('/api/candidates', (req, res) => {
-  const sql = `SELECT * FROM candidates`;
+  const sql = `SELECT candidates.*, parties.name 
+             AS party_name 
+             FROM candidates 
+             LEFT JOIN parties 
+             ON candidates.party_id = parties.id`;
   const params = [];
   db.all(sql, params, (err, rows) => {
     if (err) {
